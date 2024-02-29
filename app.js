fa7add2e36f1b1572fe8cd306f489831db22e316
Johnny Smith <156276958+jrsmithma17@users.noreply.github.com>
2024-02-28 19:21:14 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-123
+
+function getName(){
+  const usersName = prompt("What is your name?");
+  // declaring a constant variable called usersName, assigning it the value of what the prompt returns (the user's input)
+  return usersName;
+}
